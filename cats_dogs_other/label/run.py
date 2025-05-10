e53c970c9602b4fb0871f5bd1f674cfe9ea2c8bd
ayb010 <147661868+ayb010@users.noreply.github.com>
2025-05-10 07:16:13 +0000

    partie 4

--- cats_dogs_other/label/run.py
+++ cats_dogs_other/label/run.py
@@ -1,4 +1,11 @@
+import os
+from extraction import extract_images
 
 if __name__ == "__main__":
-    # Faire le code ici
-    print('')
+    # Préparation du dataset
+    raw_folder = "cats_dogs_other/label/dataset/_raw"
+    postprocess_folder = "cats_dogs_other/label/dataset/extract"
+    if not os.path.isdir(postprocess_folder) or not os.listdir(postprocess_folder):
+        res = extract_images(raw_folder, postprocess_folder)
+        print("files input = " + str(res.number_files_input))
+        print("files output = " + str(res.number_images_output))
